6caa0a2365da9dc3878601e1886ee2b4bb073ab1
Regona <128967729+regonadev@users.noreply.github.com>
2023-04-02 10:05:00 +0300

    ✨

---
+++ install.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Installing packages...
+pip install -r requirements.txt
+echo.
+echo --------------------------------------
+echo          Packages installed!          
+echo --------------------------------------
+echo.
+pause
